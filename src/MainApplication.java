2c2ee823b750653de82c33249836dfdefb8c848b
lzy77_ <channelforletsplayshik@gmail.com>
2021-11-29 23:02:24 +0300

    ADD MainApplication class, empty GornerTableFrame class

---
+++ src/MainApplication.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("NO ARGUMENTS");
+            System.exit(-1);
+        }
+
+        double[] coefficients = new double[args.length];
+        int i = 0;
+        try {
+            for (String arg: args) {
+                coefficients[i++] = Double.parseDouble(arg);
+            }
+        } catch (NumberFormatException exception) {
+            System.out.println("WRONG ARGUMENTS");
+            System.exit(-2);
+        }
+
+        GornerTableFrame frame = new GornerTableFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
