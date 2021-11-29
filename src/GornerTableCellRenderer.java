1ae80d3d8e2b39688b1d4040141c3f636765ab39
lzy77_ <channelforletsplayshik@gmail.com>
2021-11-30 00:03:12 +0300

    ADD empty window in GornerTableFrame, empty GornerTableModel class and empty GornerTableCellRenderer class

---
+++ src/GornerTableCellRenderer.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+import java.awt.*;
+
+public class GornerTableCellRenderer implements TableCellRenderer
+{
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        return null;
+    }
+}
--- src/GornerTableFrame.java
+++ src/GornerTableFrame.java
@@ -1,6 +1,24 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GornerTableFrame extends JFrame
 {
+    private static final int WIDTH = 1280;
+    private static final int HEIGHT = 720;
 
+    private double[] coefficients;
+
+    private GornerTableCellRenderer renderer = new GornerTableCellRenderer();
+
+    private GornerTableModel dataTable;
+
+    GornerTableFrame(double[] coefficients) {
+        super("ТАБУЛИРОВАНИЕ ПО СХЕМЕ ГОРНЕРА");
+        this.coefficients = coefficients;
+        setSize(WIDTH, HEIGHT);
+
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        setLocation((tk.getScreenSize().width - WIDTH) / 2, (tk.getScreenSize().height - HEIGHT) / 2);
+
+    }
 }
