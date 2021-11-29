316d8682b89e27fb422af2306c2ee1b3e3cd3ed6
lzy77_ <channelforletsplayshik@gmail.com>
2021-11-30 01:25:50 +0300

    ADD menu with ability to save file in .txt, .bin, .csv; table renderer realisation, search for value in table, search for primes, "about program" menu

--- src/GornerTableModel.java
+++ src/GornerTableModel.java
@@ -1,7 +1,7 @@
 import javax.swing.table.AbstractTableModel;
 
-public class GornerTableModel extends AbstractTableModel
-{
+public class GornerTableModel extends AbstractTableModel {
+
     private double[] coefficients;
     private double xBeg;
     private double xEnd;
@@ -13,18 +13,94 @@ public class GornerTableModel extends AbstractTableModel
         this.step = step;
         this.coefficients = coefficients;
     }
+
     @Override
     public int getRowCount() {
-        return 0;
+        return (int) (Math.ceil((xEnd - xBeg) / step) + 1);
     }
 
     @Override
     public int getColumnCount() {
-        return 0;
+        return 4;
     }
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        return null;
+        double x = xBeg + rowIndex * step;
+        double result = 0;
+        double alternativeResult = 0;
+
+        if (columnIndex == 1 || columnIndex == 3) {
+            int i = coefficients.length - 1;
+            result = coefficients[i--];
+            while (i >= 0) {
+                result = result * x + coefficients[i--];
+            }
+        }
+
+        if (columnIndex == 2 || columnIndex == 3) {
+            int i = coefficients.length - 1;
+            alternativeResult = 0;
+            while (i >= 0) {
+                alternativeResult += coefficients[i] * Math.pow(x, i);
+                i--;
+            }
+        }
+
+        switch (columnIndex) {
+            case 0: {
+                return x;
+            }
+            case 1: {
+                return result;
+            }
+            case 2: {
+                return alternativeResult;
+            }
+            case 3: {
+                return Math.abs(result - alternativeResult);
+            }
+            default: {
+                return 0;
+            }
+        }
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        switch (column) {
+            case 0: {
+                return "Значение X";
+            }
+            case 1: {
+                return "Значение многочлена по схеме Горнера";
+            }
+            case 2: {
+                return "Значение многочлена возведением в степень";
+            }
+            case 3: {
+                return "Разница";
+            }
+            default: {
+                return "";
+            }
+        }
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        return Double.class;
+    }
+
+    public double getXBeg() {
+        return xBeg;
+    }
+
+    public double getXEnd() {
+        return xEnd;
+    }
+
+    public double getStep() {
+        return step;
     }
 }
